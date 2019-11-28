17cff9eda5365c3f0b35d767dc0f00eac8887a26
song jiang <song@tigera.io>
2019-11-28 13:53:41 +0000

    Add one more worker for ipvs tests.

--- calico/create_cluster.sh
+++ calico/create_cluster.sh
@@ -40,6 +40,7 @@ nodes:
 - role: control-plane
 - role: worker
 - role: worker
+- role: worker
 kubeadmConfigPatches:
 - |
   apiVersion: kubeadm.k8s.io/v1beta2
@@ -63,6 +64,7 @@ echo "Set ipv6 address on each node"
 docker exec kind-control-plane ip -6 a a 2001:20::8/64 dev eth0
 docker exec kind-worker ip -6 a a 2001:20::1/64 dev eth0
 docker exec kind-worker2 ip -6 a a 2001:20::2/64 dev eth0
+docker exec kind-worker3 ip -6 a a 2001:20::3/64 dev eth0
 echo
 
 echo "Install Calico for dualstack"
